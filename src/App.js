bc68921c32617d35d328710399b5e80e3be1bd1e
Dhrunit <jimmyprajapati99@gmail.com>
2020-11-02 20:02:59 +0530

    Added a navbar and styles and also some hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+
+
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route path='/' component={Navbar} />
+			</Switch>
+		</Router>
 	);
 }
 
