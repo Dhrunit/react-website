7f9bf4eb0fac6bfd95c86d36b0461ec59cd9ceb4
Dhrunit <jimmyprajapati99@gmail.com>
2020-11-06 19:50:12 +0530

    Completed the website

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/pages/Footer/Footer';
 
 function App() {
-	return (
-		<Router>
-			<Switch>
-				<Route path='/' component={Navbar} />
-			</Switch>
-		</Router>
-	);
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/sign-up' exact component={SignUp} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
